8ce4442cb1e92a1c02e2494c5d89be3239333e67
Felipe Quérette <felipe.querette@gmail.com>
2024-07-19 22:37:50 -0300

    fill out doctor entity

--- src/main/java/br/senai/example/doctor_registration/entities/DoctorEntity.java
+++ src/main/java/br/senai/example/doctor_registration/entities/DoctorEntity.java
@@ -1,10 +1,7 @@
 package br.senai.example.doctor_registration.entities;
 
 import br.senai.example.doctor_registration.enums.EspecialidadeEnum;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
@@ -12,10 +9,14 @@ import java.time.LocalDate;
 public class DoctorEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_doctor")
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
+    @Column(nullable = false, unique = true)
     private String crm;
 
     private LocalDate dataNascimento;
@@ -25,4 +26,63 @@ public class DoctorEntity {
     @Enumerated(value = EnumType.STRING)
     private EspecialidadeEnum especialidade;
 
+    public DoctorEntity() {
+    }
+
+    public DoctorEntity(Long id, String nome, String crm, LocalDate dataNascimento, String telefone, EspecialidadeEnum especialidade) {
+        this.id = id;
+        this.nome = nome;
+        this.crm = crm;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.especialidade = especialidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public EspecialidadeEnum getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(EspecialidadeEnum especialidade) {
+        this.especialidade = especialidade;
+    }
 }
