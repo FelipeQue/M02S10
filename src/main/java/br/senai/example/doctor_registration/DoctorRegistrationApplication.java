fc590183d730d257ba378a003779341f5b080bcd
Felipe Quérette <felipe.querette@gmail.com>
2024-07-19 21:33:49 -0300

    first commit

---
+++ src/main/java/br/senai/example/doctor_registration/DoctorRegistrationApplication.java
@@ -0,0 +1,13 @@
+package br.senai.example.doctor_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorRegistrationApplication.class, args);
+	}
+
+}
